5acef8f60d854c77b6380480ed465b73e472b2e0
Tolu Taiwo <54937174+Lunarr0@users.noreply.github.com>
2024-09-14 08:29:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var quoteList = [
-    "No! You're not fat, just stupid.",
+   "No! You're not fat, just stupid.",
     "Hahaha, not wibe, It's vibe mf",
     "You can't rush perfection Barbie chill, you got this!!",
     "SHUT UP! You're good enough",
     "Yes Barbie!!! You have muscles, shut up",
-    "You are not fat mf!!!! Don't be dumb"
+    "You are not fat mf!!!! Don't be dumb",
+    "Gyonyoru Vagy",
+    "csinos vagy",
+    "Megbabonáz a szemed.",
+    "Szia, cica! Van gazdád?",
+    "Nagyon szeretlek",
+    "Meg tudod csinálni!",
+    "Gyerünk bazdmeg.",
+    "Mit veszíthetsz?",
+    "Ne add fel!",
+    "Higgy magadban!",
+    "Ez nagyon szeretelek",
+    "Gyonyoru Vagy, meg nagyon szeretlek",
+    "Ez az! let's go!",
     
 ];
 
